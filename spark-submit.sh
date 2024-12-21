f1536f4292face26eec81ba5520dc3a5ee19459f
wopoadi <kodjo@osekoo.com>
2024-12-21 18:18:45 +0100

    remove generated files

--- spark-submit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-spark-submit \
-    --deploy-mode client \
-    --master "$SPARK_MASTER_URL" \
-    --executor-cores 4 \
-    --executor-memory 2G \
-    --num-executors 1 \
-    --class "MainApp" \
-    "target/scala-2.12/hands-on-spark-streaming_2.12-0.1.jar" \
